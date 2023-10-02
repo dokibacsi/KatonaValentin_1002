f51f8a76aa5cde7273881b75f8fa475987423c87
dokibacsi <kvalcsi02@gmail.com>
2023-10-02 09:00:20 +0200

    bekeres() kesz es felig a feltolt();

--- src/kiccica/Kiccica.java
+++ src/kiccica/Kiccica.java
@@ -1,20 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package kiccica;
 
-/**
- *
- * @author katona.v.fabio
- */
-public class Kiccica {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Kiccica {
+    Scanner scn = new Scanner(System.in);
+    String[] pakli = new String[22];
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Kiccica kc = new Kiccica();
+        kc.inito();
     }
     
+    private void inito(){
+        bekeres();
+        pakliFeltolt();
+        kiirat();
+        ezVolt();
+    }
+
+    private void bekeres() {
+        System.out.println("Add meg az oszlop számát: (1-3)");
+        int szam = scn.nextInt();
+        boolean ertekKozott = szam >= 1 && szam <= 3;
+        
+        while(!ertekKozott){
+            System.out.println("Add meg az oszlop számát: (1-3)");
+            szam = scn.nextInt();
+            ertekKozott = szam >= 1 && szam <= 3;
+        }
+    }
+    
+    private void pakliFeltolt(){
+        String[] szinek = {"Piros", "Tök", "Zöld", "Makk"};
+        String[] jelek = {"Ász", "Felső", "Alsó", "X", "IX", "VIII"};
+        
+        for (int i = 0; i < szinek.length; i++) {
+            for (int j = 0; j < jelek.length; j++) {
+                pakli[i] += szinek[i]+jelek[j];
+            }
+        }
+        
+        System.out.println(pakli);
+        
+    }
+    
+    private void kiirat() {
+        
+    }
+
+    private void ezVolt() {
+        
+    }
 }
