d0f028a4e1e7b69baede056195a44a640c0c4475
dokibacsi <kvalcsi02@gmail.com>
2023-10-02 09:30:33 +0200

    keveres() megiras es ezVolt() atiras

--- src/pakliuj/PakliUj.java
+++ src/pakliuj/PakliUj.java
@@ -1,20 +1,21 @@
-package kiccica;
+package pakliuj;
 
 import java.util.Scanner;
 
-public class Kiccica {
+public class PakliUj {
     Scanner scn = new Scanner(System.in);
     String[] pakli = new String[22];
 
     public static void main(String[] args) {
-        Kiccica kc = new Kiccica();
+        PakliUj kc = new PakliUj();
         kc.inito();
     }
     
     private void inito(){
-        bekeres();
         pakliFeltolt();
         kiirat();
+        bekeres();
+        keveres();
         ezVolt();
     }
 
@@ -39,16 +40,46 @@ public class Kiccica {
                 pakli[i] += szinek[i]+jelek[j];
             }
         }
-        
-        System.out.println(pakli);
-        
     }
     
     private void kiirat() {
-        
+        for (int i = 0; i < pakli.length; i++) {
+            if(i % 3 == 0){
+                System.out.print("\n");
+            }
+            System.out.print(pakli[i]);
+        }
     }
 
-    private void ezVolt() {
+    private void keveres() {
+        String[] tomb = new String[22];
         
+        switch(i){
+            case 1:
+                for (int i = 0; i < pakli.length; i++) {
+                    tomb[7] = 20-(i-1)*3;
+                    tomb[7-14] = 19-(i-1)*3;
+                    tomb[14-21] = 21-(i-1)*3;
+                }
+            break;
+            case 2:
+                for (int i = 0; i < pakli.length; i++) {
+                    tomb[7] = 19-(i-1)*3;
+                    tomb[7-14] = 21-(i-1)*3;
+                    tomb[14-21] = 20-(i-1)*3;
+                }
+            break;
+            case 3:
+                for (int i = 0; i < pakli.length; i++) {
+                    tomb[7] = 20-(i-1)*3;
+                    tomb[7-14] = 21-(i-1)*3;
+                    tomb[14-21] = 19-(i-1)*3;
+                }
+            break;
+        }
+    }
+    
+    private void ezVolt(){
+        System.out.println("A kigondolt lap ez volt:");
     }
 }
